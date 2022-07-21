a83980593cda4dd159c667464343a8390bf67770
RyanCheungJF <ryan9cheung@gmail.com>
2022-07-22 01:13:52 +0800

    Add quarter logic and tests

--- insights/quarter.py
+++ insights/quarter.py
@@ -1,2 +1,4 @@
 def to_quarter(month: int):
-    raise Exception
+    if month < 1 or month > 12:
+        raise Exception
+    return f"Q{((month - 1) // 3) + 1}"
--- test/test_quarter.py
+++ test/test_quarter.py
@@ -4,5 +4,24 @@ from insights.quarter import to_quarter
 
 
 class Test(TestCase):
-    def test_january(self):
-        self.assertEqual('Q1', to_quarter(1))
+    def test_to_quarter_validMonth(self):
+        dateDict = {
+            1: 'Q1',
+            2: 'Q1',
+            3: 'Q1',
+            4: 'Q2',
+            5: 'Q2',
+            6: 'Q2',
+            7: 'Q3', 
+            8: 'Q3', 
+            9: 'Q3',
+            10: 'Q4', 
+            11: 'Q4',
+            12: 'Q4' 
+        }
+        for k, v in dateDict.items():
+            with self.subTest(f'to_quarter({k}) = {v}'):
+                self.assertEqual(v, to_quarter(k))
+
+    def test_to_quarter_invalidMonth(self):
+        self.assertRaises(Exception, to_quarter, 0)
